efc3caa30e97d48957b102f4b93cea25b47576e5
sdpercussion <sdpercussion@gmail.com>
2016-10-01 23:02:16 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+//button that changes the paragraph
+function clickyButton() {
+   document.getElementById("demo").innerHTML = "Paragraph changed.";
+};
+//second button
+function secondButton() {
+  document.getElementById("demo2").innerHTML = "This one changed too."
+};
